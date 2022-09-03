9e792ba4a9c5c88a4039293d8e8fc5a7c1ebdb74
Tal Koren <ardethian@gmail.com>
2022-09-03 18:13:25 +0300

    fixing ch12 urls

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ export const TRACKING_EVENTS = {
 
 /* eslint-disable max-len */
 const CHANNEL_11_URL = '//kan11.media.kan.org.il/hls/live/2024514/2024514/master.m3u8';
-const CHANNEL_12_URL = '//keshethlslive-i.akamaihd.net/hls/live/512033/CH2LIVE_HIGH/index.m3u8';
-const CHANNEL_12_TICKET_PREFETCH_URL = '//mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/i/CH12LIVE_HIGH/index.m3u8?b=200-2500&rv=AKAMAI';
+const CHANNEL_12_URL = '//mako-streaming.akamaized.net/direct/hls/live/2033791/k12dvr/index.m3u8';
+const CHANNEL_12_TICKET_PREFETCH_URL = '//mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/stream/hls/live/2033791/k12dvr/index.m3u8?b-in-range=800-2700&rv=AKAMAI';
 const CHANNEL_13_URL = '//d18b0e6mopany4.cloudfront.net/out/v1/08bc71cf0a0f4712b6b03c732b0e6d25/index.m3u8';
 const SEINFELD_URL = '//seinfeld.b-cdn.net/hls/seinfeld/src/index.m3u8';
 
