79b38bf5fa4edf18a0d630864597c8056127a81e
Tal Koren <ardethian@gmail.com>
2023-02-24 01:36:17 +0200

    switching video url with watchseinfeld.net url because CORS :(

--- src/config.js
+++ src/config.js
@@ -12,7 +12,7 @@ const CHANNEL_11_URL = '//kan11.media.kan.org.il/hls/live/2024514/2024514/master
 const CHANNEL_12_URL = '//mako-streaming.akamaized.net/direct/hls/live/2033791/k12dvr/index.m3u8';
 const CHANNEL_12_TICKET_PREFETCH_URL = '//mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/stream/hls/live/2033791/k12dvr/index.m3u8?b-in-range=800-2700&rv=AKAMAI';
 const CHANNEL_13_URL = '//d18b0e6mopany4.cloudfront.net/out/v1/08bc71cf0a0f4712b6b03c732b0e6d25/index.m3u8';
-const SEINFELD_URL = '//seinfeld.b-cdn.net/hls/seinfeld/src/index.m3u8';
+const SEINFELD_URL = '//watchseinfeld.net';
 
 export const channels = [
   {
@@ -43,6 +43,7 @@ export const channels = [
   },
   {
     name: 'Seinfeld',
-    url: SEINFELD_URL
+    url: SEINFELD_URL,
+    type: 'iframe'
   }
 ];
