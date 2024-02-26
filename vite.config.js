4db9dae7c6b3684820d197fe32935f42c490bb64
Tal Koren <tal.koren@knostic.ai>
2024-02-26 15:03:18 +0200

    env

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig((command, mode) => {
+export default defineConfig((command) => {
   return {
     // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
-    base: mode === "production" ? "/tv/" : "/",
+    base: command.mode === "production" ? "/tv/" : "/",
     define: {
       "process.env": {},
     },
