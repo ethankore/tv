e48a56980fe5833eaf21f0f9f62fd9881bf52498
Ethan Koren <ardethian@gmail.com>
2024-06-13 01:47:04 +0300

    Adding i24news

--- src/config.js
+++ src/config.js
@@ -12,6 +12,7 @@ const CHANNEL_11_URL = 'https://kan11.media.kan.org.il/hls/live/2024514/2024514/
 const CHANNEL_12_URL = 'https://mako-streaming.akamaized.net/stream/hls/live/2033791/k12dvr/index.m3u8';
 const CHANNEL_12_TICKET_PREFETCH_URL = 'https://mass.mako.co.il/ClicksStatistics/entitlementsServicesV2.jsp?et=ngt&lp=/stream/hls/live/2033791/k12dvr/index.m3u8?b-in-range=800-2700&rv=AKAMAI';
 const CHANNEL_13_URL = 'https://d18b0e6mopany4.cloudfront.net/out/v1/08bc71cf0a0f4712b6b03c732b0e6d25/index.m3u8';
+const I24_NEWS_URL = 'https://bcovlive-a.akamaihd.net/6e3dd61ac4c34d6f8fb9698b565b9f50/eu-central-1/5377161796001/556d51d2f08440b3a4b6442b43008aa0/playlist-all_dvr_ssaiM.m3u8';
 const SEINFELD_URL = 'https://demo.streamplanet.tv/hls/seinfeld/src/index.m3u8';
 
 export const channels = [
@@ -40,6 +41,10 @@ export const channels = [
   {
     name: 'כאן 11',
     url: CHANNEL_11_URL
+  },
+  {
+    name: 'i24News',
+    url: I24_NEWS_URL
   },
   {
     name: 'Seinfeld',
